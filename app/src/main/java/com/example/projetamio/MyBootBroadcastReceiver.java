2f652a905c6bc81cdbd385e05cdae07275da57b7
Quentin Millardet <quentin.millardet@telecomnancy.eu>
2020-10-06 12:11:41 +0200

    TP1 : Version finalisée du TP1

---
+++ app/src/main/java/com/example/projetamio/MyBootBroadcastReceiver.java
@@ -0,0 +1,34 @@
+package com.example.projetamio;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.util.Log;
+
+import static com.example.projetamio.MainActivity.PREFS_NAME;
+
+public class MyBootBroadcastReceiver extends BroadcastReceiver {
+
+    public MyBootBroadcastReceiver(){
+        super();
+        Log.d(this.getClass().getName(), "Controleur initialisé");
+    }
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Log.d("MyBootBroadcastReceiver", "Le broadcast est bien arrivé");
+        SharedPreferences settings = context.getSharedPreferences(PREFS_NAME, 0);
+        if ( settings.getBoolean("startAtBoot", false)){
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(new Intent(context, MainService.class));
+            } else {
+                context.startService(new Intent(context, MainService.class));
+            }
+        }
+        else{
+            Log.d("MyBootBroadcastReceiver","Le service ne devait pas être démaré");
+        }
+    }
+}
